0119e720fb4701dbaa55aeeb954998688dca08e3
takyisky_yt <emmanuelcrash945@gmail.com>
2025-08-05 08:39:41 +0000

    Instance and Method Overriding task completed

---
+++ InheritanceAndMethodOverloading/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace InheritanceAndMethodOverloading
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Dog class inherits from Animal
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Cat class to also inherit from animal and overide
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+
+            Console.WriteLine("Meow");
+        }
+
+    }
+
+    // Main program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testing the animal class 
+            Animal genericAnimal = new Animal();
+            genericAnimal.MakeSound();
+
+            //Testing the dog class
+            Dog dog = new Dog();
+            dog.MakeSound();
+
+            //Testing the cat class
+            Cat cat = new Cat();
+            cat.MakeSound();
+        }
+    }
+}
