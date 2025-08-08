90c5f10458842053db01654fd859de8254c4ce37
takyisky_yt <emmanuelcrash945@gmail.com>
2025-08-08 18:45:48 +0000

    Completed the Interface Demo Task

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    // Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Classes implementing the interface
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();       // Car is moving
+            bicycle.Move();   // Bicycle is moving
+        }
+    }
+}
